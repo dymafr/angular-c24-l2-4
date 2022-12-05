c433a4e4bf4b63931af0338e5b4d94df05d28495
dymafr <32801166+dymafr@users.noreply.github.com>
2022-12-05 12:39:33 +0100

    2 files modified

--- src/app/exemple.ts
+++ src/app/exemple.ts
@@ -1,32 +1,29 @@
-import { COMMA, ENTER } from "@angular/cdk/keycodes";
-import { Component } from "@angular/core";
-import { MatChipInputEvent } from "@angular/material/chips";
+import { COMMA, ENTER } from '@angular/cdk/keycodes';
+import { Component } from '@angular/core';
+import { MatChipInputEvent } from '@angular/material/chips';
 
 @Component({
-  selector: "app-exemple-component",
-  templateUrl: "exemple.html",
-  styleUrls: ["exemple.scss"]
+  selector: 'app-exemple-component',
+  templateUrl: 'exemple.html',
+  styleUrls: ['exemple.scss'],
 })
 export class ExempleComponent {
   // entrée, virgule
   readonly separateurs: number[] = [ENTER, COMMA];
 
-  vetements = [{ nom: "Chemise" }, { nom: "Veste" }, { nom: "Chapeau" }];
+  vetements = [{ nom: 'Chemise' }, { nom: 'Veste' }, { nom: 'Chapeau' }];
 
   add(event: MatChipInputEvent): void {
-    let input = event.input;
-    let value = event.value;
+    const value = (event.value || '').trim();
 
     if (value) {
-      this.vetements.push({ nom: value.trim() });
+      this.vetements.push({ nom: value });
     }
 
-    if (input) {
-      input.value = "";
-    }
+    event.chipInput!.clear();
   }
 
   remove(vetement: any): void {
-    this.vetements = this.vetements.filter(v => v != vetement);
+    this.vetements = this.vetements.filter((v) => v != vetement);
   }
 }
